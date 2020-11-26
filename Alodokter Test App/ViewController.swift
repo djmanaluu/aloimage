ad9bd20d2360aefec8c33076b15f1dd251ce4472
David Jourdan Manalu <david.jourdan@traveloka.com>
2020-11-26 09:03:57 +0700

    Initial Commit

---
+++ Alodokter Test App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alodokter Test App
+//
+//  Created by David Jordan Manalu on 26/11/20.
+//  Copyright © 2020 David Jordan Manalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
